dc5cf7fe5b6973b520d32a4b294ad30dcb24e141
Mitchell Jepsergon Ngetich <mitchell.ngetich@student.moringaschool.com>
2022-09-02 15:59:19 +0300

    update

--- code.js
+++ code.js
@@ -7,7 +7,7 @@ let userBtn = document.getElementById('btn')
 let object = document.getElementById('object');
 
 
-// findResult.addEventListener('click', getMeal);
+ findResult.addEventListener('click', getMealRecipe);
 userBtn.addEventListener('click', () => {
     fetch(url)
     .then(resp => resp.json())
@@ -20,36 +20,76 @@ userBtn.addEventListener('click', () => {
                 <img src=${meal.strMealThumb} alt="">
                 <div class="item-container">
                     <h1 class="sub-title"> ${meal.strMeal}</h1>
-                    <h1 id="sub-title"> Instructions:</h1>
-                    <h1 class="sub-title"> ${meal.strInstructions} </h1>
-                    <a class="view-button" href=${meal.strInstructions} target="_blank">Get Recipe</a>
+                    <h1 class="sub-title" id="idmeal"> ${meal.idMeal}</h1>
+                <button class="view-button" id=${meal.idMeal}>Get Recipe</button>
+                
                 </div>
-                <p class="object-data"></p>
+                <p class="object-data">${meal.strInstructions}</p>
             </div>
             `
+            document.addEventListener('DOMContentLoaded', () => {
+                document.querySelector('.view-button').addEventListener('click', () => {
+                    let paragraph = document.querySelector('.object-data');
+                    paragraph.style.display = 'none';
+    
+                })
+
+            })
+            
         })
         findResult.innerHTML = html;
-        let getRecipe = document.querySelector('.view-button')
-        getRecipe.addEventListener('click', () => {
-            let popButton = document.getElementById('popup-btn');
-            popButton.addEventListener('click', () => {
-                
+        // let getRecipe = document.querySelector('.view-button')
+        // getRecipe.addEventListener('click', () => {
+        
+    }  
             })
         })
+    
+         
+        
+            function getMealRecipe(e){
+               e.preventDefault();
+                 if(e.target.classList.contains('view-button')){
+                    console.log(e.target);
+                    let id = e.target.getAttribute('id');
+                    
+                
+                    console.log(id);
+                    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+                    .then(response => response.json())
+                .then(data =>{ 
+                    console.log('data', data);
+                   
+
+                    mealRecipeModal(data.meals) 
+                })
+                    .catch(error => console.log('error', error));
+                
+               }
+            }
+        function mealRecipeModal(meal){
+            console.log(meal);
+            meal = meal[0];
+            let html = 
+            `
+            <h2 class="recipe-title">$(meal.strMeal)</h2>
+                <p class="meal-category">${meal.strCategory}</p>
+                <div clas="meal-instruction">
+                    <h3>Instructions:</h3>
+                    <p>${meal.strInstructions}</p>
+                    <img src=${meal.strMealThumb} alt="">
+                    <button class="view-button" id=${meal.strInstructions}>Get Recipe</a>
+                    <div id="view-recipe">hddh</div>
+                 </div>
+            
+            `;
+            //mealDetailsContent.innerHTML = html;
+            // mealDetailsContent.getAttribute.classList.addEventListener
+            // ('showRecipe');
         }
-    })
-           
-         })
-            // function getMeal(e){
-            //     e.preventDefault();
-            //     if(e.target.classList.contains('view-button')){
-            //         let mealItem = e.target.parentElement;
-            //         console.log(mealItem);
-            //     }
-            // }
     
 
- 
+
 
 
 
